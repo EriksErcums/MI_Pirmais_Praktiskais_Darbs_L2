837ce2245dfa7debd519cc34683f9d176fd956b0
Desk <188556274+DefaultUser-png@users.noreply.github.com>
2026-03-09 11:52:58 +0200

    Menu correctly assigns and remembers first turn

--- Menu/Menu.gd
+++ Menu/Menu.gd
@@ -11,8 +11,8 @@ const GAME_PATH: String = "res://Game/GameLoop.tscn"
 # functions too, but they are slower and more inconvinient
 @export var slider_cells: HSlider
 @export var label_cells: Label
-@export var checkbutton_turn: CheckButton
-@export var checkbutton_algo: CheckButton
+@export var checkbtn_turn: CheckButton
+@export var checkbtn_algo: CheckButton
 @export var button_play: Button
 
 var cells: int = 15
@@ -22,12 +22,12 @@ var pruning: bool = false
 func _ready() -> void:
 	label_cells.text = str(cells)
 	slider_cells.value = cells
-	checkbutton_turn.button_pressed = turn
-	checkbutton_algo.button_pressed = pruning
+	checkbtn_turn.button_pressed = turn
+	checkbtn_algo.button_pressed = pruning
 	
 	slider_cells.value_changed.connect(_on_slider_changed)
-	checkbutton_turn.toggled.connect(_on_checkbtn_turn_toggled)
-	checkbutton_algo.toggled.connect(_on_checkbtn_algo_toggled)
+	checkbtn_turn.toggled.connect(_on_checkbtn_turn_toggled)
+	checkbtn_algo.toggled.connect(_on_checkbtn_algo_toggled)
 	button_play.pressed.connect(_on_play_pressed)
 	
 	button_play.grab_focus()
@@ -47,6 +47,7 @@ func _on_play_pressed() -> void:
 	var game: GameLoop = game_scene.instantiate()
 	game.cells_max = cells
 	game.turn = turn
+	game.init_turn = turn
 	game.pruning = pruning
 	get_parent().add_child(game)
 	queue_free()
