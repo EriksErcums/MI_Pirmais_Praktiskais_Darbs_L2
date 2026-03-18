7d09bec35a3e3762fb021145a9574aabe48a3994
oxonomii <grejs1704@gmail.com>
2026-03-18 23:23:45 +0200

    gameTree refactoring / built-in generate_children()

--- Common/GameTree.gd
+++ Common/GameTree.gd
@@ -3,28 +3,33 @@ class_name GameTree extends RefCounted
 const INF: int = 1 << 30
 
 var states: Array[State]
-# Dictionary[int, Array[int]] not supported | (solution: PackedInt32Array)
-var verticies: Dictionary[int, Array]
+var verticies: Dictionary[int, PackedInt32Array]
 
-func _init(init_state: State, depth: int = -1) -> void:
+func _init(init_state: State, depth: int = -1, is_p1_turn: bool = true) -> void:
 	states = []
 	verticies = {}
 	var root_state = init_state.clone()
 	states.append(root_state)
 	if depth > 0:
-		build_tree(0, depth)
+		build_tree(0, depth, is_p1_turn)
 
-func build_tree(state_id: int, depth: int) -> void:
+func build_tree(state_id: int, depth: int, is_p1_turn: bool) -> void:
 	if depth == 0:
 		return
 	var state = states[state_id]
-	state.generate_children()
+	# built-in generate_children() //////////////////
+	var children = []
+	if state.nums.size() > 1:
+		for i in range(state.nums.size() - 1):
+			var child = state.clone()
+			child.process_turn(i, i + 1, not is_p1_turn)
+			children.append(child)
 	verticies[state_id] = []
-	for child in state.children:
+	for child in children:
 		var child_id = states.size()
 		states.append(child)
 		verticies[state_id].append(child_id)
-		build_tree(child_id, depth - 1)
+		build_tree(child_id, depth - 1, not is_p1_turn)
 
 
 func add_state(state: State) -> void:
